d49101d6275b5222c87d43fec7ed1efc6195ddc5
Ky Sopheareak <reakphea0802@gmail.com>
2025-05-26 17:01:00 +0700

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,23 +2,27 @@ import { Routes } from '@angular/router';
 import { NavMenuComponent } from './routes/nav-menu/nav-menu.component';
 
 export const routes: Routes = [
-    {
-        path: 'login',
-        loadComponent: () => import('./routes/login/login.component').then(c => c.LoginComponent)
-    },
-    {
-        path: '',
-        component: NavMenuComponent,
-        children: [
-            {
-                path: '',
-                redirectTo: 'list',
-                pathMatch: 'full',
-            },
-            {
-                path: 'list',
-                loadComponent: () => import('./routes/list/routes/list.component').then(c => c.ListComponent)
-            }
-        ]
-    }
+  {
+    path: '**',
+    loadComponent: () => import('./routes/login/login.component').then(c => c.LoginComponent)
+  },
+  {
+      path: 'login',
+      loadComponent: () => import('./routes/login/login.component').then(c => c.LoginComponent)
+  },
+  {
+      path: '',
+      component: NavMenuComponent,
+      children: [
+          {
+              path: '',
+              redirectTo: 'list',
+              pathMatch: 'full',
+          },
+          {
+              path: 'list',
+              loadComponent: () => import('./routes/list/routes/list.component').then(c => c.ListComponent)
+          }
+      ]
+  }
 ];
